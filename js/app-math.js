efe19793f27a9bd8ae734ba396f056f018693dd2
tanyaweaver <tanya83@uw.edu>
2016-04-29 15:11:43 -0700

    everything execpt the clear table button

---
+++ js/app-math.js
@@ -0,0 +1,49 @@
+var submissionForm = document.getElementById('submission-form');
+var unitsArray = [];
+var userOutput = document.getElementById('user-output');
+var resultsDisplay = document.getElementById('results');
+
+var results = [];
+
+function Convert (name, conversion, inputUnits, outputUnits){
+  this.name1 = name;
+  this.conversion = conversion;
+  this.inputUnits = inputUnits;
+  this.outputUnits = outputUnits;
+  unitsArray.push(this);
+}
+
+var inchToCm = new Convert('inch-cm', 2.54, ' inch ', ' centimeter ');
+var ozToMl = new Convert('fl.oz-ml', 29.574, ' fluid ounces ', ' milliliters ');
+var miToKm = new Convert('mi-km', 1.60934, ' miles ', ' kilometers ');
+var lbToKg = new Convert('lb-kg', 0.453592, ' pounds ', ' kilograms ');
+var shitToshit = new Convert('shit-tonn', 1240, ' standard shit-tonn ', ' metric shit-tonn ');
+
+function renderResults(){
+  resultsDisplay.textContent = '';
+  for(var i = 0; i < results.length; i++){
+    var p = document.createElement('p');
+    p.textContent = results[i];
+    resultsDisplay.appendChild(p);
+  }
+}
+
+function handleSubmit(event){
+  var userSelect = document.getElementById('user-select');
+  // console.log('userSelect.value ' + userSelect.value);
+  var userInput = parseInt(event.target.input1.value);
+  event.preventDefault();
+  // console.log('userInput ' + userInput);
+  for(var i = 0; i < unitsArray.length; i++){
+    if(userSelect.value == unitsArray[i].name1){
+      var answer = parseFloat(userInput * unitsArray[i].conversion).toFixed(3);
+      // console.log('answer ' + answer);
+      userOutput.textContent = (userInput + unitsArray[i].inputUnits + 'equals ' + answer + unitsArray[i].outputUnits);
+      results.push(userOutput.textContent);
+    }
+  }
+  renderResults();
+  userInput = null;
+}
+
+submissionForm.addEventListener('submit', handleSubmit);
